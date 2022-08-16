d7d3d07915c3bb3d5b1fc9255860e1517391ac39
lyubomir penchev <lubomir.penchev@oxid-sales.com>
2022-08-16 13:52:24 +0300

    Add styling

--- src/screens/ShoppingLists.js
+++ src/screens/ShoppingLists.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
+import styles from './../styles/styles'
 const ShoppingList = () => {
-return (
-	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-	<Text style={{ color: "#006600", fontSize: 40 }}>ShoppingList Screen!</Text>
-	<Ionicons name="md-home" size={80} color="#006600" />
-	</View>
-);
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>ShoppingList Screen!</Text>
+			<Ionicons name="md-home" size={80} color="#006600" />
+		</View>
+	);
 };
 
 export default ShoppingList;
