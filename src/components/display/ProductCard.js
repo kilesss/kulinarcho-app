8112ce7782bb18243799afc507779c482cd2eb13
Boyan67 <mep19zju@uea.ac.uk>
2022-09-07 12:40:56 +0300

    Final touches

--- src/components/display/ProductCard.js
+++ src/components/display/ProductCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import {View, Text, TouchableOpacity, TouchableWithoutFeedback} from 'react-native'
+import {View, Text, TouchableOpacity, TouchableWithoutFeedback, Image} from 'react-native'
 import {stylesRecipes} from "../../styles/stylesRecipes";
 import styles from "../../styles/styles";
 import {MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
+import Images from "../../../public/images";
 
 
-export const ProductCard = ({ title, textRight, icon, iconColor, onPress}) => (
+export const ProductCard = ({ title, textRight, image, onPress}) => (
     <TouchableWithoutFeedback onPress={onPress}>
         <View style={stylesRecipes.productCard}>
         <View style={stylesRecipes.productCardIconTitle}>
-            <MaterialCommunityIcons name={icon} size={30} color={iconColor}/>
+            <Image source={image} style={{height: 30, width: 30}}/>
             <Text style={{fontSize: 16, fontWeight:"bold", color:"#4B4C4C", paddingLeft: 7}}>{title}</Text>
         </View>
 
